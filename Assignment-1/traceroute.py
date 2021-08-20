d2329bf209c727930b848aacadf010c9e4afa5dc
Prakhar Aggarwal <cs5190441@iitd.ac.in>
2021-08-20 18:37:10 +0530

    partial report

--- Assignment-1/traceroute.py
+++ Assignment-1/traceroute.py
@@ -6,43 +6,43 @@ import socket
 import time
 import matplotlib.pyplot as plt
 
-MAX_HOPS = 64
-# HOST = socket.gethostbyname(socket.gethostname())
+MAX_HOPS = 30
+TIMEOUT = 0.4
 HOST = ''
+# HOST = socket.gethostbyname(socket.gethostname())
 
-def traceroute(dest_addr, max_hops=MAX_HOPS, timeout=0.2):
-    proto_icmp = socket.getprotobyname('icmp')
-    proto_udp = socket.getprotobyname('udp')
-    port = 33434
+def tr(host_addr, max_hops = MAX_HOPS, timeout = TIMEOUT):
+    
+    icmp = socket.getprotobyname('icmp')
+    udp = socket.getprotobyname('udp')
+    PORT = 5000
 
     for ttl in range(1, max_hops+1):
         initial_time = time.time()
-        rx = socket.socket(socket.AF_INET, socket.SOCK_RAW, proto_icmp)
-        rx.settimeout(timeout)
-        rx.bind((HOST, port))
+        receive = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
+        receive.settimeout(timeout)
+        receive.bind((HOST, PORT))
         
-        tx = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, proto_udp)
-        tx.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
-        tx.sendto(b'Hello', (dest_addr, port))
-        
+        transmit = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, udp)
+        transmit.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
+        transmit.sendto(b'Hello World!', (host_addr, PORT))
 
         try:
             ending_time = time.time()
             elapsed_time = str((ending_time - initial_time)*100000)[:4] + "ms"
-            data, curr_addr = rx.recvfrom(512)
-            # print(curr_addr, type(curr_addr))
+            _, curr_addr = receive.recvfrom(512)
             curr_addr = curr_addr[0]
         except socket.error:
             curr_addr = "Request timed out"
             elapsed_time = "  *  "
         finally:
-            rx.close()
-            tx.close()
+            receive.close()
+            transmit.close()
 
         yield (curr_addr, elapsed_time) # yield is a keyword that returns from the function without destroying the state of it's local variables.
 
-        if curr_addr == dest_addr:
-            break
+        if curr_addr == host_addr:
+            return
 
 def graph(data, dest_name, dest_addr):
     plt.plot(data)
@@ -51,17 +51,36 @@ def graph(data, dest_name, dest_addr):
     plt.show()    
 
 if __name__ == "__main__":
-    dest_name = sys.argv[1]
-    # dest_name = "www.google.com"
-    dest_addr = socket.gethostbyname(dest_name)
-    print("traceroute to {} ({}), {} hops max".format(dest_name, dest_addr, MAX_HOPS))
+    hostname = ""
+    found = False
+    
+    try:
+        hostname = sys.argv[1]
+    except:        
+        while hostname == "":
+            print("Hostname:",end="")
+            hostname = input()
+    
+    while found == False:
+        try:
+            host_addr = socket.gethostbyname(hostname)
+            found = True
+        except:
+            print("Incorrect hostname")
+            print("Hostname:",end="")
+            hostname = input()
+            while hostname == "":
+                print("Hostname:",end="")
+                hostname = input()
+                        
+    print("\ntraceroute to {} ({}), {} hops max".format(hostname, host_addr, MAX_HOPS))
     
     data = []
     
-    for i, v in enumerate(traceroute(dest_addr)):
-        print("{}\t{}\t\t{}".format(i+1, v[1], v[0]))
-        if(v[1] == "  *  "):
+    for i, tup in enumerate(tr(host_addr)):
+        print("{}\t{}\t\t{}".format(i+1, tup[1], tup[0]))
+        if(tup[1] == "  *  "):
             data.append(0)
         else:
-            data.append(float(v[1][:len(v[1])-2]))
-    graph(data, dest_name, dest_addr)
+            data.append(float(tup[1][:len(tup[1])-2]))
+    graph(data, hostname, host_addr)
